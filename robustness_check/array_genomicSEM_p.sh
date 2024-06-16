ef2228a1966cb1c00b351dc3566f2ab1162c44cb
lwjy3142 <liaowangjingyi@outlook.com>
2024-06-16 22:07:33 +0100

    modified:   robustness_check/array_genomicSEM_p.sh

--- robustness_check/array_genomicSEM_p.sh
+++ robustness_check/array_genomicSEM_p.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -l
-#SBATCH --output=/scratch/prj/teds/Sumstate_PRS_Codelab/disease_specificity/robustness_check/p_gwas/p_genomicSEM1_%A_%a.out
-#SBATCH --error=/scratch/prj/teds/Sumstate_PRS_Codelab/disease_specificity/robustness_check/p_gwas/p_genomicSEM1_%A_%a.err
 #SBATCH --time=2-00:00:00
 #SBATCH --cpus-per-task=32
 #SBATCH --ntasks=1
@@ -8,7 +6,7 @@
 #SBATCH --array=2-39
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --partition=cpu
-#SBATCH --job-name=p
+#SBATCH --job-name=array_genoSEM
 #SBATCH --mail-type=END
 #SBATCH --mail-user=wangjingyi.liao@kcl.ac.uk
 
@@ -18,7 +16,7 @@ export OPENBLAS_NUM_THREADS=1
 #the array argument should reflect the number of row in the row_chunk.csv
 
 
-WORKdir='/scratch/prj/teds/Sumstate_PRS_Codelab/disease_specificity/robustness_check' #set working directory
+WORKdir='./disease_specificity/genomicSEM/' #set working directory
 
 cd $WORKdir
 
@@ -27,10 +25,9 @@ end_row=$(awk -F',' "NR==$SLURM_ARRAY_TASK_ID {print \$2}" ${WORKdir}/row_chunk.
 
 
 #load necessary modules 
-module load test_switch_kcl/2.0.0-gcc-13.2.0
-source test_switch
-module load  r/4.1.1-gcc-11.4.0-withx-rmath-standalone-python3+-chk-version 
+module load r/4.1.1-gcc-9.4.0-withx-rmath-standalone-python-3.8.12
 
 #Execute program located in current folder
+#this is a example running the r script to do the GWAS, the R script should be seperated into individual scripts and submitted seperately as jobs per trait
 
-Rscript --vanilla /scratch/prj/teds/Sumstate_PRS_Codelab/disease_specificity/robustness_check/p_genomicSEM.R -s $start_row -e $end_row
+Rscript --vanilla ./disease_specificity/genomicSEM/P_genomicSEM_meta.R -s $start_row -e $end_row
